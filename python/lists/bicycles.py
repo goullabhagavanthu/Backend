ad4dcf23f8c4516bb2bce70b649895469a6b96d1
Goulla Bhagavanthu <goullabhagavanthu@gmail.com>
2025-10-16 07:49:02 +0000

    delated files

--- python/lists/bicycles.py
+++
@@ -1,8 +0,0 @@
-bicycles = ['trek', 'cannondaele', 'redline','specialized']
-print(bicycles[1])
-print(bicycles[3])
-print(bicycles[-1])
-
-message = f"my first bicycle was a {bicycles[0].title()}."
-
-print(message)
